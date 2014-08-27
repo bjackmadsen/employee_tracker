8bcbf64104eb612f8533b02bdf3c75f93126302c
Catherine Tran & Brandon Madsen <student@epicodus.com>
2014-08-27 11:23:58 -0700

    pass spec to show functionality of employees belonging to a division.

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,2 +1,11 @@
 require 'spec_helper'
 
+describe Employee do
+  it "belongs to a division" do
+    division = Division.create({:name => "Sales"})
+    employee = Employee.create({:name => "Cat", :division_id => division.id})
+    expect(employee.division).to eq division
+  end
+end
+
+
